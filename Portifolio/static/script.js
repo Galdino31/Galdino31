a59ba3a95eea85ad761be2441552031813a3f184
Rogério Galdino <138232285+Galdino31@users.noreply.github.com>
2025-05-20 16:10:50 -0300

    Add files via upload

---
+++ Portifolio/static/script.js
@@ -0,0 +1,6 @@
+
+//script.js
+//exemplo de código JavaScript que pode ser adicionado aqui 
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Portifolio carregado com sucesso!');
+});
