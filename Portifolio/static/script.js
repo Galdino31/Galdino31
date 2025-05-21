aaf53be6bbbf5137288aa82337488d649db699b5
Rogério Galdino <138232285+Galdino31@users.noreply.github.com>
2025-05-21 12:14:08 -0300

    Delete Portifolio directory

--- Portifolio/static/script.js
+++
@@ -1,6 +0,0 @@
-
-//script.js
-//exemplo de código JavaScript que pode ser adicionado aqui 
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Portifolio carregado com sucesso!');
-});
